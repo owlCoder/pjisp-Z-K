9c655e7efda1b03fd58c2c51dcf574f0fb5483ac
owlCoder <danijel.xda@gmail.com>
2020-11-11 22:08:35 +0100

    Hypermarket - Init

---
+++ PJISP - Zadaci - T3 i T4/04 Hipermarket/hipermarket.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_NIZ 100
+
+struct artikal_st {
+    double cena;
+    int brojProdatihArtikala;
+    double trgovackaMarza;
+    char naziv[20 + 1];
+} artikli[MAX_NIZ];
+
+struct zarada_st {
+    char naziv[20 + 1];
+    double ostvarenaZarada;
+} analizaZarade[MAX_NIZ];
+
+FILE *otvoriDatoteku(char *, char *);
+int ucitajPodatke(FILE *);
+void zarada(int);
+void upisiIzvestaj(FILE *, int);
+
+int main(int argc, char **argm)
+{
+    if(argc != 3)
+    {
+		printf("USAGE: %s IN_FILENAME OUT_FILENAME\n", argm[0]);
+		exit(42);
+    }
+    else {
+
+        FILE *datoteka = otvoriDatoteku(argm[1], "r");
+        FILE *upis = otvoriDatoteku(argm[2], "w");
+
+        int i = ucitajPodatke(datoteka);
+
+        zarada(i);
+        upisiIzvestaj(upis, i);
+
+        fclose(datoteka);
+        fclose(upis);
+
+        return 0;
+    }
+}
+
+FILE *otvoriDatoteku(char *naziv, char *rezim) {
+    FILE *datoteka;
+
+    datoteka = fopen(naziv, rezim);
+
+    if(datoteka == NULL) {
+        printf("Nije moguce otvoriti datoteku -> \"%s\"\n", naziv);
+        exit(0xda7);
+    }
+    else
+        return datoteka;
+}
+
+int ucitajPodatke(FILE *datoteka)
+{
+	int i = 0;
+	rewind(datoteka);
+    while( (fscanf(datoteka, "%lf %d %lf %s",
+               &artikli[i].cena,
+               &artikli[i].brojProdatihArtikala,
+               &artikli[i].trgovackaMarza,
+               &artikli[i].naziv)) != EOF) {
+        i++;
+    }
+	return i;
+}
+
+void zarada(int i)
+{
+    int j;
+	for(j = 0; j < i; j++)
+	{
+		strcpy(analizaZarade[j].naziv,
+			   artikli[j].naziv);
+
+        analizaZarade[j].ostvarenaZarada = (
+                (artikli[j].cena * artikli[j].brojProdatihArtikala)
+                * (artikli[j].trgovackaMarza / 100) );
+	}
+}
+
+void upisiIzvestaj(FILE *upis, int i)
+{
+	int j;
+	for(j = 0; j < i; j++)
+        fprintf(upis, "%7.2lf %s\n",
+                analizaZarade[j].ostvarenaZarada,
+                analizaZarade[j].naziv);
+}
