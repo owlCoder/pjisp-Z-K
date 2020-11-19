23da9bf440911789a55c5e8c6cae2f2303c996c0
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2020-11-19 18:08:14 +0100

    Finish it
    Bug fixes and improvements

--- PJISP - Zadaci - T1 i T2/09 Neparni susedi/neparni_susedi.c
+++ PJISP - Zadaci - T1 i T2/09 Neparni susedi/neparni_susedi.c
@@ -3,11 +3,12 @@
 
 #define MAX_ARRAY_SIZE 10
 
-int zbir(int);
+unsigned is_neparni_sused(int *, int, int);
 
 int main()
 {
     int A[MAX_ARRAY_SIZE], zbirC[MAX_ARRAY_SIZE], i, j, count = 0, n;
+    unsigned jes = 0;
 
     do {
         printf("\nUnesite velicinu niza [2 >= n <= %d]\n>> ", MAX_ARRAY_SIZE);
@@ -21,23 +22,28 @@ int main()
     }
     for(i = 0; i < n; i++)
         printf("\n\tA[%d] = %d", i, A[i]);
-    
+
     puts("");
 
-    for(i = 0; i < n; i++)
-        printf("\n\tzbir(A[%d]) = %d", i, zbirC[i]);
-        
-    puts("");
-
-    for(i = 0; i < n - 1; i++)
-        for(j = i + 1; j < n; j++) {
-            if(zbirC[i] == zbirC[j]) { 
-                printf("\n\tzbir(A[%d]) == zbir(A[%d]) == % d", i, j, zbirC[i]);
-                count++;
-            }
+    for(i = 0; i < n - 1; i++){
+        if( jes = is_neparni_sused(A, i, i + 1)) { 
+            printf("\n\todd(A[%d]) == odd(A[%d]) == %d", i, i + 1, jes);
+             count++;
+        }
     }
 
     printf("\n\n\tcount = %d\n\n", count);
 
     return 0;
+}
+
+unsigned is_neparni_sused(int *A, int i, int j)
+{
+    unsigned nep1 = A[i] % 2 != 0 ? 1 : 0;
+    unsigned nep2 = A[j] % 2 != 0 ? 1 : 0;
+    
+    if(nep1 == 1 && nep2 == 1)
+        return 1;
+    else 
+        return 0;
 }
