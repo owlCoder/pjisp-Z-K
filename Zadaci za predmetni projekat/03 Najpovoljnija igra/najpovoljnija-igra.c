8628271a29ef606c11a8da0c2952ef382de89d79
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2021-01-01 14:29:55 +0100

    Data load and list init... Done...

---
+++ Zadaci za predmetni projekat/03 Najpovoljnija igra/najpovoljnija-igra.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_IGRA      10 + 1
+#define MAX_ZANR       8 + 1
+#define MAX_PLATFORMA  8 + 1
+
+typedef struct igra_st {
+    char nazivIgre[MAX_IGRA], zanr[MAX_ZANR], platforma[MAX_PLATFORMA];
+    double cena;
+    struct igra_st *sledeci;
+} IGRE;
+
+void init_list(IGRE **);
+IGRE *create_item(const char *, const char *, const char *, const double);
+void add_to_list(IGRE **, IGRE *);
+void load_data(FILE *, IGRE **);
+FILE *fopen_dat(char *, char *);
+
+int main(int argn, char **args)
+{
+    IGRE *g;
+    
+    FILE *in = fopen_dat(args[3], "r");
+    FILE *out = fopen_dat(args[4], "w");
+
+    init_list(&g);
+    //load_data(in, &g);
+    add_to_list(&g, create_item("djksfds", "fdsfdsfd", "gdfsgsdsg", 55.55) );
+    fclose(in);
+    fclose(out);
+
+    return 0;
+}
+
+void init_list(IGRE **g) { *g = NULL; }
+
+IGRE *create_item(const char *nazivIgre, const char *zanr, const char *platforma, const double cena)
+{
+    IGRE *new = (IGRE *) malloc(sizeof(IGRE));
+
+    if(new == NULL)
+    {
+        printf("\nAllocation failed!\tError code: %s\n\n", "0xN0MEM0RY");
+        exit(42);
+    }
+
+    strcpy(new -> nazivIgre, nazivIgre);
+    strcpy(new -> zanr, zanr);
+    strcpy(new -> platforma, platforma);
+    new -> cena = cena;
+    new -> sledeci = NULL;
+
+    return new;
+}
+
+void add_to_list(IGRE **g, IGRE *new)
+{
+    if(*g != NULL) {
+        add_to_list(&( (*g) -> sledeci), new);
+    }
+    else {
+        *g = new;
+        return;
+    }
+}
+
+void load_data(FILE *in, IGRE **g)
+{
+    IGRE load;
+    while(fscanf(in, "%s %s %s %lf", load.nazivIgre, load.zanr, load.platforma, &load.cena) != EOF) {
+        add_to_list(g, create_item(load.nazivIgre, load.zanr, load.platforma, load.cena));
+    }
+}
+
+FILE *fopen_dat(char *name, char *mode)
+{
+    FILE *f = fopen(name, mode);
+
+    if(f == NULL)
+    {
+        printf("\nFile doesn't exist!\tError code: %s\n\n", "0xN0F1LE");
+        exit(45);
+    }
+    return f;
+}
