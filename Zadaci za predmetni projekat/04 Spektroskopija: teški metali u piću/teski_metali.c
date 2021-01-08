6d8c832c8b99047b914460cb6a1f08b83e8c5293
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2021-01-08 09:29:07 +0100

    Init blank code for start project

---
+++ Zadaci za predmetni projekat/04 Spektroskopija: teški metali u piću/teski_metali.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIMBOL    2 + 1
+#define MAX_STR      20 + 1
+
+typedef struct element_st {
+    char simbol[MAX_SIMBOL];
+    char ime[MAX_STR];
+    int atomska_t;
+    char vrsta[MAX_SIMBOL];
+    struct element_st *s;
+} ELEMENT;
+
+int main(int argn, char **args)
+{
+    if(argn != 4) {
+        puts("%s VRSTA IN_FILENAME OUT_FILENAME");
+        exit(11);
+    }
+    ELEMENT *e;
+    
+    return 0;
+}
